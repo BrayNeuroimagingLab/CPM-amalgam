565ebb29eee3499ec4298cb9d987c387fcc8322f
Rylan Marianchuk <45247316+rylan-marianchuk@users.noreply.github.com>
2021-07-20 23:24:32 -0600

    Update flatten3d.py

--- src/flatten3d.py
+++ src/flatten3d.py
@@ -1,4 +1,5 @@
 """
+Preprocessing file for missing data and .mat files
 Convert 3D Functional Connectivity object into 2D flattened matrix
 
 No constraint on parcellation size
@@ -35,8 +36,20 @@ for x in FC:
             sbj.append(x[i,j])
     FC_flat.append(sbj)
 
+FC_flat = np.array(FC_flat, dtype=np.float64)
+
+# Remove columns containing nan
+bad_inds = set(np.argwhere(np.isnan(FC_flat))[:,1])
+print("Number of edges to remove because of nan: " + str(len(bad_inds)))
+bad_inds = sorted(bad_inds)
+print("FC_flat shape before deletion: " + str(FC_flat.shape))
+FC_flat = np.delete(FC_flat, np.s_[bad_inds], axis=1)
+print("FC_flat shape after deletion: " + str(FC_flat.shape))
+print("Checking for nan: ")
+print(set(np.argwhere(np.isnan(FC_flat))))
+
 with open('../FC_flat.npy', 'wb') as f:
-    np.save(f, np.array(FC_flat, dtype=np.float64))
+    np.save(f, np.array(FC_flat))
 
 with open('../target.npy', 'wb') as f:
     np.save(f, np.array(Y, dtype=np.float64))
